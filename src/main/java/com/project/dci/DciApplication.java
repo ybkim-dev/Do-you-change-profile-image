7bcf9cea2c9a61fdb3f6df0338c3a4c99d97bc58
ybkim-dev <beanybigggy@gmail.com>
2022-12-29 12:56:04 +0900

    init dci project (Do you change profile image?)

---
+++ src/main/java/com/project/dci/DciApplication.java
@@ -0,0 +1,13 @@
+package com.project.dci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/dci/DciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
