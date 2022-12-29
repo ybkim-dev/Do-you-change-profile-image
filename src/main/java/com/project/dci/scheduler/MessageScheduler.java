dc357ac3af35a9615ca3e28671e86bbc99b2cd90
ybkim-dev <beanybigggy@gmail.com>
2022-12-30 01:19:15 +0900

    feat : 스케줄러를 통해 30분마다 한 번씩 수행하도록 설정

---
+++ src/main/java/com/project/dci/scheduler/MessageScheduler.java
@@ -0,0 +1,24 @@
+package com.project.dci.scheduler;
+
+import com.project.dci.web.controller.api.MessageController;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class MessageScheduler {
+    @Value("${config.api.send-message.query}")
+    private String query;
+    private final MessageController messageController;
+
+    public MessageScheduler(MessageController messageController) {
+        this.messageController = messageController;
+    }
+
+    @Scheduled(cron = "*/30 * * * * *")
+    public void sendMessage() {
+        messageController.getUsers(query);
+    }
+}
